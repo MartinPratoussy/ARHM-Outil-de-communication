e1be3c1553e18f2081e709df1a1b125906c4f533
Martin Pratoussy <martin.pratoussy@orange.fr>
2020-01-24 09:44:22 +0100

    Ajout de descriptions sur les éléments de la classe "user"

--- user.h
+++ user.h
@@ -3,7 +3,7 @@
 
 #include <QDate>
 
-
+// Classe représentant un utlisateur de l'application
 class user
 {
 private:
@@ -17,11 +17,14 @@ public:
     user(QString firstname, QString lastname, QDate birthDate, QString handicap);
     ~user();
 
+    // Affiche l'interface personnalisée de l'utlisateur
     void DisplayInterface();
+    // Edite les différents attributs de l'utlisateur
     void EditFirstname(QString firstname);
     void EditLastname(QString lastname);
     void EditBirthDate(QDate birthDate);
     void EditHandicap(QString handicap);
+    // Supprime l'utlisateur
     void Delete();
 };
 
