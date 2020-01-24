ed5a2ff21d488ab77080f6db9583548e1ec02d13
Martin Pratoussy <martin.pratoussy@orange.fr>
2020-01-24 09:36:24 +0100

    Description de la classe "interface"

---
+++ interface.h
@@ -0,0 +1,15 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+#include "pictogram.h"
+
+class interface
+{
+private:
+    Pictogram pictos[100];
+    QString sentence;
+public:
+    interface();
+};
+
+#endif // INTERFACE_H
