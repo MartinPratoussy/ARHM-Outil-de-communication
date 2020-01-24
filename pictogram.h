7dd25170fde4d8c2db139e4708355f10b1b744b9
Martin Pratoussy <martin.pratoussy@orange.fr>
2020-01-24 09:49:02 +0100

    Ajout de descriptions pour la classe "pictogram"

--- pictogram.h
+++ pictogram.h
@@ -4,11 +4,13 @@
 #include <QtTextToSpeech/QTextToSpeech>
 #include <QPixmap>
 
+// Pictogrammes sur lesquels l'utlisateur devra appuyer pour construire ses phrases (unb texte, un son et une image seront liés à chaque élément)
 class Pictogram
 {
 private:
     QString definition;
     QPixmap image;
+    // Pointeur permettant de synthétiser vocalement la définition
     QTextToSpeech * speech = new QTextToSpeech();
 public:
     Pictogram();
